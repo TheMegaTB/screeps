1e7b88cbdcc7a34da266ec5b835d3ad912ce3149
Til Blechschmidt <til@blechschmidt.de>
2016-10-03 23:28:52 +0200

    Fixed clean task

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
                 src: ['dist/**.js']
             }
         },
-        clean: 'dist/'
+        clean: ['dist/']
     });
 
     grunt.loadNpmTasks('grunt-screeps');
